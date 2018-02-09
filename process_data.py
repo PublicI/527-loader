9be013ac4c3ffefef3ac2cfa2010040c0a81c6f2
Chris Zubak-Skees <chriszs@gmail.com>
2018-02-08 21:59:56 -0500

    WIP

--- process_data.py
+++ process_data.py
@@ -1,49 +1,74 @@
-# The purpose of this script is to process 527 organizations' 8872 header (committee information), 8872 schedule A (contributions) and 8872 schedule B (expenditures) records.
+# This script prepares bulk data from the IRS collecting filings by 527 organizations
+# for loading into a database.
 
 data_file = "var/IRS/data/scripts/pofd/download/FullDataFile.txt"
 infile = open(data_file,"r", encoding="utf-8")
 
-# Create the files
-outfile_headers = open("8872_headers.txt", "w")
-outfile_a = open("8872_schedule_a.txt", "w")
-outfile_b = open("8872_schedule_b.txt", "w")
+outfiles = {}
 
-linecount = 0 # Set the line count at zero prior to writing data to the outfiles
+# Create the files
+# outfile_headers = open("8872_headers.txt", "w")
+# outfile_a = open("8872_schedule_a.txt", "w")
+# outfile_b = open("8872_schedule_b.txt", "w")
+
+full_line = ""
+append = False
 
 for line in infile: # Read the file line by line
-    linecount += 1 # Increase the line count by one as each line is written
-
     # Remove carriage returns
     line = line.replace("\r","")
     line = line.replace("\n", "")
     line = line.replace("\\", "")
-    values = line.split("|") # Separate the column values by pipe
+
+    if append:
+        full_line += line
+    else:
+        full_line = line
+
+    append = False
+
+    values = full_line.split("|") # Separate the column values by pipe
     linetype = values[0] # Use the value of the first column to determine the line type
+
+    # ignore status messages and multi-line descriptions
+    # if len(linetype) > 2:
+    #     continue
 
     # fix problem lines
     count = len(values)
-    expected = 18
-    if (linetype == "A" or linetype == "B") and count != expected:
-        difference = expected - count # Calculate the number of missing pipes
-        pipes = ["|"] * difference
-        filler = "".join(pipes)
-        line = line + filler # Add the missing pipes to the end of the problem line
+    #expected = 18
+    #if linetype == "A" or linetype == "B":
+    #    expected = 18
+    #else if linetype == "H":
+    #    expected = 4
+    #else if linetype == "1":
+    #    expected = 44
+    #else if linetype == "D" or linetype == 'R':
+    #    expected = 13
+    #else if linetype == "":
+
+    #if count < expected:
+    #    append = True
+    #    continue
+
+    print(linetype + "," + str(count))
+
+    # if (linetype == "A" or linetype == "B") and count > expected:
+    #    print(full_line)
+
+        # difference = expected - count # Calculate the number of missing pipes
+        # pipes = ["|"] * difference
+        # filler = "".join(pipes)
+        # print(line)
+        # line = line + filler # Add the missing pipes to the end of the problem line
 
     if line[-1:] == '|':
         line = line[:-1]
 
-    # Populate the files
-    if linetype == "2":
-        outfile_headers.write(line)
-        outfile_headers.write("\n")
+    # if linetype not in outfiles:
+        # outfiles[linetype] = open("line_" + linetype + ".txt", "w")
 
-    elif linetype == "A":
-        outfile_a.write(line)
-        outfile_a.write("\n")
+    #outfiles[linetype].write(line)
+    #outfiles[linetype].write("\n")
 
-    elif linetype == "B":
-        outfile_b.write(line)
-        outfile_b.write("\n")
-
-print("Data processed.")
 
