d3de4b4b20356d0dc2147345a240104055343874
Chris Zubak-Skees <chriszs@gmail.com>
2018-12-01 12:58:34 -0500

    Drone-ify

---
+++ load.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+wget https://forms.irs.gov/app/pod/dataDownload/fullData
+unzip fullData
+python process_data.py
+psql -f create.sql
+psql -c "COPY irs527_filings FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < "line_2.txt"
+psql -c "COPY irs527_contributions FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < "line_A.txt"
+psql -c "COPY irs527_expenditures FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < "line_B.txt"
+rm -rf var
+rm *.txt
+rm fullData
